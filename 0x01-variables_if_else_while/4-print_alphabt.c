2e64583e7fd3249a8bb16b54d183489f9f0815b1
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-24 10:15:26 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
