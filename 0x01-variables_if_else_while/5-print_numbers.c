47e081bfa8114df7eb638bfbeef95df275a4722c
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-24 10:29:18 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -prints allsingle digit numbers of 
+ * base 10 from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int Num;
+
+	for (Num = 0; Num < 10; Num++)
+	{
+		printf("%d", Num);
+	}
+	putchar('\n');
+	return (0);
+}
