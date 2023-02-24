f72ad06beb4040aef7429298c19b9d6d9a26bffa
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-23 21:07:38 -0800

    alx-low_level_programming

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 int main (void){
-
-  int n;
-  
+  int n; 
     srand(time(0));
       n = rand() - RAND_MAX / 2;
    if (n>0)
@@ -15,6 +13,4 @@ int main (void){
 	  printf("%d is negative\n", n); 
 
    return(0);
-
-
 }
