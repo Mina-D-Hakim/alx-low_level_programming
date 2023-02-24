cf0228188104c752ab14769dac22581a7299cfca
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-24 08:44:18 -0800

    alx-low_level_programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase, followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	{
+		putchar(C);
+	}
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
