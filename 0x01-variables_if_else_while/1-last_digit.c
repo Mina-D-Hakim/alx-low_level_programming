0e736b83c3e835f0c890222f74ad4ea1dedddd2e
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-24 05:31:51 -0800

    alx-low_level_programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main funcation - positive_or_negative
- * Description:  print the last digit of the number stored in the variable n.
+ * main - print the last digit of the number stored in the variable n
  * Return: 0
  */
 int main(void)
