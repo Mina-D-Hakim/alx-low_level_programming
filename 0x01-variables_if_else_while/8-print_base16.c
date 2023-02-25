08ebe03ec6bd3bcd7fbc176c00dac92f69c32f2c
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-24 20:20:29 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -  Print base 16 numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(chd);
+		ch++;
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
