f0380d52051dbb4878898586b764dd488cb80715
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-02-27 06:57:10 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Test wheter  c is lowercase
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
