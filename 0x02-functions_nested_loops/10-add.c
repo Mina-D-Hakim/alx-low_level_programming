aae38895b824cc73cadbf2bd415a7f75dbffc63e
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-01 02:30:10 -0800

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers .
+ * @x: First paramater
+ * @y: Second paramater
+ * @s:sum of returns the result
+ */
+
+int add(int x, int y)
+{
+	int s;
+
+	s = x + y;
+	return (s);
+}
