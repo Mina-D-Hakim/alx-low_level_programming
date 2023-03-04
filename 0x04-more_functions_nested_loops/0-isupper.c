98915c78932fed6671944be2e5a4b43dcc2ccb71
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-04 07:55:15 -0800

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,8 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return 1 if true, 0 if false
- * On error, -1 return.
- */
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
