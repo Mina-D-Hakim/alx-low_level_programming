359f9226fbfa2a6e0d0bdf71f6f52389c765e3e9
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-04 00:05:28 -0800

    0x03-debugging

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -5,23 +5,6 @@
  * Return: 0
 */
 
-int positive_or_negative(int n)
-{
-
-    if (n > 0)
-       pintf("%d is positive\n", n);
-
-
-    else if (n < 0)
-       printf("%d is negative\n", n);
-
-    else
-       printf("%d is zero\n", n);
-
-
-    return (0);
-
-}
 
 int main(void)
 {
