bfa16022f0b84b873414b4554d0a23b62c5875e1
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-04 00:03:38 -0800

    0x03-debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,4 +1,4 @@
-#include "mainh"
+#include "main.h"
 
 /**
  * main - entry point
@@ -13,7 +13,9 @@ int positive_or_negative(int n)
 
     else if (n < 0)
        printf("%d is negative\n", n);
-     else printf("%d is zero\n", n);
+
+    else
+       printf("%d is zero\n", n);
 
     return (0);
 
