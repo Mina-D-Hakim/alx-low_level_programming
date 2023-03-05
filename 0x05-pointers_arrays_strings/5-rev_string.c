b63a76522c87bd81ff6f8780f2ff22a9ace9f7d6
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-05 00:56:16 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * rev_string - This fuction print a string reverser
+ * rev_string - This fuction
+ * print a string reverser
  *
  *@s: this is the pointer that point to a string
  */
