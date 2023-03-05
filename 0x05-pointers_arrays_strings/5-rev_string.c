657a62d21a5a042ebeb0886eaf77d742e38f1887
Mina-D-Hakim <mina.dawoud@onewaygroups.com>
2023-03-05 01:08:54 -0800

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,23 @@
 
 void rev_string(char *s)
 {
-	int a, len;
+	int length, j, i;
+	char v1, v2;
 
-	char *begin, *end = s;
-
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		end++;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
-	{  char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
+
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
 	}
-	end[len + 1] = '\0';
 }
